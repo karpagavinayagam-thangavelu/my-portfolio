ea8a2f34f1adfdd0e4aa36499aea84d551f01af8
karpagavinayagam thangavelu <karpagavinayagam.thangavelu@gmail.com>
2023-11-16 23:33:35 +0530

    deploy

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,6 +1,7 @@
 
 export const env = {
-     imagePath : "/my-portfolio/assets/images"
+     imagePath : "/my-portfolio/assets/images",
+     PUBLIC_URL : "/my-portfolio"
     // Local
     // imagePath : "/assets/images"
 }
